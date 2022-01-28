97bc561bd0599c4ddd492d9a2fca981c4ad12589
Permlap Phola <permlapteam@gmail.com>
2022-01-28 20:13:40 +0700

    Nav bar fixed

--- viclass/src/Main/About.js
+++ viclass/src/Main/About.js
@@ -2,21 +2,24 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-        <div>
-            <ul>
-                <li class="text-3xl">About Us</li>
-                <li>We are a tool for supporting teachers who desire to build up
+    //   Hero section abtout
+   <section class="relative  mt-40 Superhight w-full bgYellow ">
+    <div class=" ml-10  mt-20 absolute width52 h-96 colorWhite drop-shadow-lg px-5 rounded-3xl ">
+      <ul class="flex flex-col text-lef ">
+        <li class="text-5xl font-extrabold colorYellow mt-16 ml-5">About Us</li>
+        <li class="ml-8 mt-5">We are a tool for supporting teachers who desire to build up
  a classroom where students are able to both learn and enjoy 
 at the same time. We have provided many different features to 
 encourage students in participating in learning such as gamification
  in worksheets or multiple choices, the features where teachers 
 can keep tracking how are students' progress in each assignment </li>
-                <li> and more! </li>
-            </ul>
-        </div>
+      </ul>
+    </div>
+    <img class=" absolute mt-20  hight51 width52  right-0" src='/image/studentBG.png'></img>
+    
+    
 
-     </div>
+   </section>
      
      )
 }
--- viclass/src/component/HeaderList.js
+++ viclass/src/component/HeaderList.js
@@ -3,10 +3,10 @@ import "./HeaderList.css"
 function HeaderList(props) {
    
   return(
-        <div class="ml-20 mt-10" className="container2">
+        <div class="ml-10 mt-10 md:-ml-1" className="container2">
            
             <ul  className='blocklistCon'>
-                <li className="blocklist">
+                <li class="md:w-40 md:text-center" className="blocklist">
                      <img src={props.img}></img>
                      <a href="#">{props.ListName}</a>
                  </li>
