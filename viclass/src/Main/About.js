05c6ff749c9c8e55e61acaf43cc1fd89f4d3bba4
Permlap Phola <permlapteam@gmail.com>
2022-01-29 23:46:42 +0700

    Finish lading Page but with out scolling

--- viclass/src/Main/About.js
+++ viclass/src/Main/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     //   Hero section abtout
    <section class="relative md:mt-10  mt-5 h-96 md:h-96 w-screen bgYellow  ">
-    <div class="top-10 md:top-2  md:mt-10 lg:px-5 xl:p-10  z-10    h-4/6  md:w-4/6 lg:w-8/12 lg:h-5/6 md:ml-5  xl:w-4/6  xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl absolute">
+    <div class="top-10 md:top-2  md:mt-10 lg:px-5 xl:p-10  z-10     h-4/6  md:w-4/6 lg:w-8/12 lg:h-5/6 md:ml-5  xl:w-4/6  xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl absolute">
       <ul class="flex flex-col text-left  ">
-        <li class="text-5xl font-extrabold colorYellow mt-12">About Us</li>
+        <li class="text-5xl font-extrabold colorYellow mt-12 uppercase">About Us</li>
         <li class="Detail">We are a tool for supporting teachers who desire to build up
  a classroom where students are able to both learn and enjoy 
 at the same time. We have provided many different features to 
--- viclass/src/Main/LearnMore.js
+++ viclass/src/Main/LearnMore.js
@@ -3,15 +3,44 @@ import React from 'react';
 function LearnMore() {
   return (
  
-      <section class="container bg-cyan-800 w-full h-screen relative">
-            <div>
-                <ul class=" absolute right-0 md:mt-10 lg:px-10 xl:p-10  z-10  mt-5  h-80 w-full md:w-96 lg:w-8/12 lg:h-4/6 md:ml-5  xl:w-3/6 xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl md:w-69">
-                    <li class="Topic">student</li>
+      <section >
+            <div class="container bg-cyan-800 w-full h-96  md:h-96 relative lg:h-screen ">
+                <ul class=" absolute right-0 md:mt-10 lg:pb-10 xl:p-10  z-10  mt-5  h-80 w-full md:w-96 lg:w-8/12 lg:h-fit md:mr-10 xl:w-3/6 xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl ">
+                    <li class="Topic uppercase ml-10">students</li>
                     <li className='ToppicDetail'>Motivate students to be more invested in their learning and each other</li>
-                    <li className='Detail' >By exhibiting positive behavior, completing assignments, and reminding all works through application, students unlock real-life privileges, recognition to help themselves and their team and prepare themslef to hand in assigment without misinformation. Learning becomes a way to encourage students to advocate for one another and feel empowered in the classroom and in life.</li>              
+                    <li className='Detail lg:text-xl' >By exhibiting positive behavior, completing assignments, and reminding all works through application, students unlock real-life privileges, recognition to help themselves and their team and prepare themslef to hand in assigment without misinformation. Learning becomes a way to encourage students to advocate for one another and feel empowered in the classroom and in life.</li>              
                 </ul>
+            
+            <img class="hidden md:block md:h-screen max-w-full max-h-full right-100  object-contain absolute" src='/image/studeentsLearnmore.png'/>
             </div>
-            <img class="hidden md:block max-w-full max-h-full right-100  object-contain absolute" src='/image/studeentsLearnmore.png'/>
+            <div class="container BGYeloow  w-full h-96  md:h-96 relative lg:h-screen">
+                <ul class=" text-center absolute right-100 md:mt-16 lg:pb-10 md:ml-5 xl:p-10  z-10  mt-5  h-80 w-full md:w-96 lg:w-8/12 lg:h-fit md:mr-10 xl:w-3/6 xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl">
+                    <li class="Topic uppercase">Teachers</li>
+                    <li className='ToppicDetail'>Connecting with your students has never been easier</li>
+                    <li className='Detail' >Building meaningful relationships with students is the key to great classroom management. Integrating SEL and mental health into your classroom will help them nurture critical soft skills in the school day and beyond. With our help you will be easier to interact with students because we provide you lots of tools to help you create enjoyable assiment and help you to manange classroom managemnet not to be boring anymore!</li>              
+                </ul>
+            
+            <img class="hidden md:block md:h-screen max-w-full max-h-full right-0  object-contain absolute" src='/image/TeacherLearnore.png'/>
+            </div>
+            <div class="container bgYellow w-full h-96  md:h-96 relative lg:h-screen ">
+                <ul class=" absolute right-0 md:mt-10 lg:pb-10 xl:p-10  z-10  mt-5  h-80 w-full md:w-96 lg:w-8/12 lg:h-fit md:mr-10 xl:w-3/6 xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl ">
+                    <li class="Topic uppercase">Parents</li>
+                    <li className='ToppicDetail'>Participating in your childern’s learning is a a main key of education</li>
+                    <li className='Detail lg:text-xl' >Paricipating with schools and teachers is easier than you think with out Viclass’s help. In Viclass, parents are able to observe and keep tracking students’ progress and help them improve learning throgh teacher’s suggestion.</li>              
+                </ul>
+            
+            <img class="hidden md:block md:h-screen max-w-full max-h-full right-100  object-contain absolute" src='/image/parentLearnmore.png'/>
+            </div>
+            <div class="container darkblue  w-full h-96  md:h-96 relative lg:h-screen">
+                <ul class=" text-center absolute right-100 md:mt-16 lg:pb-10 md:ml-5 xl:p-10  z-10  mt-5  h-80 w-full md:w-96 lg:w-8/12 lg:h-fit md:mr-10 xl:w-3/6 xl:pt-0  colorWhite drop-shadow-lg px-5 rounded-3xl">
+                    <li class="Topic uppercase">School leaders</li>
+                    <li className='ToppicDetail'>Manage behavior at scale across all your schools</li>
+                    <li className='Detail' >In addition to experiencing schoolwide benefits, ensure continuity and consistency of your mental health, climate, and culture initiatives by unifying your approach at the district level. High-fidelity data provides clear insights into what’s working, what’s not, and how to create informed and intentional progress to support all learners.</li>              
+                </ul>
+            
+            <img class="hidden md:block md:h-screen max-w-full max-h-full right-0  object-contain absolute" src='/image/SchoolLeaderLeanmore.png'/>
+            </div>
+            
       </section>
 
   )
